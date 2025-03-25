e6dd18294602cf017df19877dfdccf786746a1e5
bobwen0205 <wenmingzhi10@gmail.com>
2025-03-25 19:55:33 -0400

    change userAuth to debug(iPhone cannot login)

--- server/middlewares/userAuth.js
+++ server/middlewares/userAuth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const userAuth = async (req, res, next) => {
     const { token } = req.cookies;
     if (!token) {
-        return res.json({ success: false, message: 'Not Authorized. Login Again' });
+        return res.json({ success: false, message: 'Not Authorized. Login Again1' });
     }
     try {
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
@@ -11,7 +11,7 @@ const userAuth = async (req, res, next) => {
         if (tokenDecode.id) {
             req.body.userId = tokenDecode.id;
         } else {
-            return res.json({ success: false, message: 'Not Authorized. Login Again' });
+            return res.json({ success: false, message: 'Not Authorized. Login Again2' });
         }
 
         next();
